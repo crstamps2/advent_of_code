0e6c23780e1d0e2a3a3feb2754b03caac775ab30
Cody Stamps <cody.stamps@hey.com>
2024-12-01 20:39:56 -0500

    initial answer for day1 puzzle

--- day1/day1.go
+++ day1/day1.go
@@ -1,5 +1,15 @@
 package day1
 
-func FindCombinedDistance() int{
-	return 1
+import (
+	"slices"
+)
+
+func FindCombinedDistance(list1 []int, list2 []int) int{
+	slices.Sort(list1)
+	slices.Sort(list2)
+	var combinedDistance int
+	for i, v := range list1 {
+		combinedDistance += v - list2[i]
+	}
+	return max(combinedDistance, -combinedDistance)
 }
