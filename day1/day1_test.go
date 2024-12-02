d46e281265443ae29e7775344b5f34a97b002569
Cody Stamps <cody.stamps@hey.com>
2024-12-01 21:10:56 -0500

    add more test cases

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -3,12 +3,22 @@ package day1
 import "testing"
 
 func TestFindCombinedDistance(t *testing.T) {
-	list1 := []int{5,1,7,6}
-	list2 := []int{9,7,5,2}
+	tests := []struct {
+		name string
+		list1 []int
+		list2 []int
+		want int
+	}{
+		{"should be 4", []int{5,1,7,6}, []int{9,7,5,2}, 4},
+		{"should be 28", []int{-5,1,7,6}, []int{9,8,5,15}, 28},
+	}
 
-	result := FindCombinedDistance(list1, list2)
-
-	if result != 4 {
-		t.Errorf("got: %v, wanted: %v", result, 4)
-	} 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := FindCombinedDistance(tt.list1, tt.list2)
+			if result != tt.want {
+				t.Errorf("got: %v, wanted: %v", result, tt.want)
+			} 
+		})
+	}
 }
