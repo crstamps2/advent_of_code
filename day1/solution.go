dd579ae0d1394face3482ea7b39bad44a1094100
Cody Stamps <cody.stamps@hey.com>
2024-12-02 23:02:52 -0500

    reorganize structure to be straight forward for advent

--- day1/solution.go
+++ day1/solution.go
@@ -1,9 +1,51 @@
-package day1
+package main
 
 import (
+	"fmt"
+	"encoding/csv"
+	"os"
+	"strconv"
 	"slices"
 	"math"
 )
+
+func main() {
+	f1, err := os.Open("list1.csv")
+	if err != nil {
+			panic(err)
+	}
+	defer f1.Close()
+
+	csvReader := csv.NewReader(f1)
+	records, err := csvReader.ReadAll()
+	var list1 []float64
+	for index, record := range records {
+		i, _ := strconv.ParseFloat(record[index], 64)
+		list1 = append(list1, i)
+	}
+	if err != nil {
+			panic(err)
+	}
+
+	f2, err := os.Open("list2.csv")
+	if err != nil {
+			panic(err)
+	}
+	defer f2.Close()
+
+	csvReader = csv.NewReader(f2)
+	records, err = csvReader.ReadAll()
+	var list2 []float64
+	for index, record := range records {
+		i, _ := strconv.ParseFloat(record[index], 64)
+		list2 = append(list2, i)
+	}
+	if err != nil {
+			panic(err)
+	}
+	
+	fmt.Printf("Your answer is: %v\n", FindCombinedDistance(list1, list2))
+}
 
 func FindCombinedDistance(list1 []float64, list2 []float64) int{
 	slices.Sort(list1)
