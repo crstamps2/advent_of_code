6867a2ad675e751e6d3d02d9d9b119bd31257f23
Cody Stamps <cody.stamps@hey.com>
2024-12-03 23:57:46 -0500

    read inputs from file, pass through report check

--- day2/solution.go
+++ day2/solution.go
@@ -1,17 +1,39 @@
 package main
 
 import (
-	// "fmt"
-	// "os"
-	// "strings"
-	// "strconv"
-	// "bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"bufio"
 	"math"
-	// "slices"
 )
 
 func main() {
+	file, err := os.Open("inputs.txt")
+	if err != nil {
+			panic(err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+
+	var stringReadings []string
+	var reports [][]int
+	for scanner.Scan() {
+		stringReadings = strings.Fields(scanner.Text())
+		var readings []int
+		for _, v := range stringReadings {
+			i, _ := strconv.Atoi(v)
+			readings = append(readings, i)
+		}
+		reports = append(reports, readings)
+	}
+	totalSafe := 0
+	for _, report := range reports {
+		if IsReportSafe(report) { totalSafe++ }
+	}
+	fmt.Printf("Number of Safe Readings: %v\n", totalSafe)
 }
 
 func IsReportSafe(readings []int) bool {
